69f8e2a68d5beddd9e88b7e33060901b01e77ad3
Denis <denis.brailovsky@gmail.com>
2020-05-01 15:10:32 +0300

    Change dev env

--- create_dev_env.sh
+++ create_dev_env.sh
@@ -5,7 +5,12 @@ cd ~
 mkdir -p .nn_log/log_example/
 mkdir -p .data_dev/data_example/
 mkdir -p .golden_repo
+mkdir -p .papers
+mkdir -p .virtualenvs
 
-cp ${run_dir}/data/data_sync_template.sh .data_dev/data_example
+
+cp ${run_dir}/data/data_sync_template.sh .data_dev/data_example/.sync.sh
+chmod u+x .data_dev/data_example/.sync.sh
+
 
 
