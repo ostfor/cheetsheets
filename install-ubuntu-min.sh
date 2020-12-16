76ad0c48c95bb1f7747307b5300229ad3db12a3a
ostfor <free_angel_@list.ru>
2020-12-16 10:01:08 +0300

    Update install-ubuntu-min.sh

--- install-ubuntu-min.sh
+++ install-ubuntu-min.sh
@@ -14,13 +14,15 @@ echo "Install packages #3..."
 sudo apt-get install fuse libfuse2
 sudo apt-get install xautomation xdotool
 
+echo "Enable alt-shift"
+dconf write /org/gnome/desktop/input-sources/xkb-options "['grp:alt_shift_toggle']"
 
 # NeoVim
 
-echo "Install NeoVim ..."
-wget --quiet https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage --output-document nvim
-chmod +x nvim
-sudo chown root:root nvim
-sudo mv nvim /usr/bin
-cd ~
-mkdir -p .config/nvim
+# echo "Install NeoVim ..."
+# wget --quiet https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage --output-document nvim
+# chmod +x nvim
+# sudo chown root:root nvim
+# sudo mv nvim /usr/bin
+# cd ~
+# mkdir -p .config/nvim
